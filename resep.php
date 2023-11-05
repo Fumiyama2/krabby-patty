e8011269e33c84ed81b011274fccda6b081fc22b
clytherenaldi <akunmenggambar2656@gmail.com>
2023-11-05 08:27:31 +0700

    edit dari fajar renaldi/clytherenaldi

--- resep.php
+++ resep.php
@@ -1,6 +1,13 @@
 <?php
 echo "Resep krabby patty";
 echo "------------------";
-echo "1. Selada";
-echo "2. Tomat";
-echo "3. Daging";
+echo "1. Bawang";
+echo "2. Ketimun";
+echo "3. Selada";
+echo "4. Tomat";
+echo "5. Daging";
+echo "6. Keju";
+echo "7. Mayonais";
+echo "8. Roti";
+echo "9. Acar";
+echo "10. Saus Tomat";
